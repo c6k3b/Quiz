7a7a957b7c6f5cdc7467d1439c856ba6f5ce49ca
AA <c6k3b@icloud.com>
2023-01-12 23:27:01 +0400

    Renaming test to clarify new intent

--- QuizEngine/QuizEngineTests/QuizTest.swift
+++ QuizEngine/QuizEngineTests/QuizTest.swift
@@ -5,41 +5,41 @@ import XCTest
 import QuizEngine
 
 class QuizTest: XCTestCase {
-    private let router = RouterSpy()
-    private var game: Game<String, String, RouterSpy>!
+    private let delegate = DelegateSpy()
+    private var quiz: Game<String, String, DelegateSpy>!
 
     override func setUp() {
         super.setUp()
-        game = startGame(
+        quiz = startGame(
             questions: ["Q1", "Q2"],
-            router: router,
+            router: delegate,
             correctAnswers: ["Q1": "A1", "Q2": "A2"]
         )
     }
 
-    func test_startGame_answerZeroOutOfTwoCorrectly_scoresZero() {
-        router.answerCallback("wrong")
-        router.answerCallback("wrong")
+    func test_startQuiz_answerZeroOutOfTwoCorrectly_scoresZero() {
+        delegate.answerCallback("wrong")
+        delegate.answerCallback("wrong")
 
-        XCTAssertEqual(router.routedResult?.score, 0)
+        XCTAssertEqual(delegate.handledResult?.score, 0)
     }
 
-    func test_startGame_answerOneOutOfTwoCorrectly_scoresOne() {
-        router.answerCallback("A1")
-        router.answerCallback("wrong")
+    func test_startQuiz_answerOneOutOfTwoCorrectly_scoresOne() {
+        delegate.answerCallback("A1")
+        delegate.answerCallback("wrong")
 
-        XCTAssertEqual(router.routedResult?.score, 1)
+        XCTAssertEqual(delegate.handledResult?.score, 1)
     }
 
-    func test_startGame_answerTwoOutOfTwoCorrectly_scoresTwo() {
-        router.answerCallback("A1")
-        router.answerCallback("A2")
+    func test_startQuiz_answerTwoOutOfTwoCorrectly_scoresTwo() {
+        delegate.answerCallback("A1")
+        delegate.answerCallback("A2")
 
-        XCTAssertEqual(router.routedResult?.score, 2)
+        XCTAssertEqual(delegate.handledResult?.score, 2)
     }
 
-    private class RouterSpy: Router {
-        var routedResult: Result<String, String>?
+    private class DelegateSpy: Router {
+        var handledResult: Result<String, String>?
         var answerCallback: (String) -> Void = { _ in }
 
         func routeTo(question: String, answerCallback: @escaping (String) -> Void) {
@@ -47,7 +47,7 @@ class QuizTest: XCTestCase {
         }
 
         func routeTo(result: Result<String, String>) {
-            routedResult = result
+            handledResult = result
         }
     }
 }
