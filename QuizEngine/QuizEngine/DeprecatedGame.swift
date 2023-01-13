d43ec11b0f6ce2b8eaf85e3ca5acc53c07f77a94
AA <c6k3b@icloud.com>
2023-01-13 23:02:39 +0400

    Removed unnecessary code

--- QuizEngine/QuizEngine/DeprecatedGame.swift
+++ QuizEngine/QuizEngine/DeprecatedGame.swift
@@ -26,9 +26,7 @@ public func startGame<Question, Answer: Equatable, R: Router>(
     router: R,
     correctAnswers: [Question: Answer]
 ) -> Game<Question, Answer, R> where R.Question == Question, R.Answer == Answer {
-    let flow = Flow(questions: questions, delegate: QuizDelegateToRouterAdapter(router, correctAnswers)) {
-        scoring($0, correctAnswers: correctAnswers)
-    }
+    let flow = Flow(questions: questions, delegate: QuizDelegateToRouterAdapter(router, correctAnswers))
     flow.start()
     return Game(flow: flow)
 }
