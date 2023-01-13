ad1d6f7cfa2aa41713d58ca98f9524d8101b8c29
AA <c6k3b@icloud.com>
2023-01-13 23:00:54 +0400

    Updated the deprecated adapter to use the new didComplete() API

--- QuizEngine/QuizEngine/DeprecatedGame.swift
+++ QuizEngine/QuizEngine/DeprecatedGame.swift
@@ -26,7 +26,7 @@ public func startGame<Question, Answer: Equatable, R: Router>(
     router: R,
     correctAnswers: [Question: Answer]
 ) -> Game<Question, Answer, R> where R.Question == Question, R.Answer == Answer {
-    let flow = Flow(questions: questions, delegate: QuizDelegateToRouterAdapter(router)) {
+    let flow = Flow(questions: questions, delegate: QuizDelegateToRouterAdapter(router, correctAnswers)) {
         scoring($0, correctAnswers: correctAnswers)
     }
     flow.start()
@@ -35,20 +35,28 @@ public func startGame<Question, Answer: Equatable, R: Router>(
 
 @available(*, deprecated)
 private class QuizDelegateToRouterAdapter<R: Router>: QuizDelegate {
-    typealias Question = R.Question
-    typealias Answer = R.Answer
-
     private let router: R
+    private let correctAnswers: [R.Question: R.Answer]
 
-    init(_ router: R) {
+    init(_ router: R, _ correctAnswers: [R.Question: R.Answer]) {
         self.router = router
+        self.correctAnswers = correctAnswers
     }
 
-    func answer(for question: Question, completion: @escaping (Answer) -> Void) {
+    func answer(for question: R.Question, completion: @escaping (R.Answer) -> Void) {
         router.routeTo(question: question, answerCallback: completion)
     }
 
-    func handle(result: Result<Question, Answer>) {
+    func didCompleteQuiz(withAnswers answers: [(question: R.Question, answer: R.Answer)]) {
+        let answersDictionary = answers.reduce([R.Question: R.Answer]()) { acc, tuple in
+            var acc = acc
+            acc[tuple.question] = tuple.answer
+            return acc
+        }
+        let score = scoring(answersDictionary, correctAnswers: correctAnswers)
+        let result = Result(answers: answersDictionary, score: score)
         router.routeTo(result: result)
     }
+
+    func handle(result: Result<R.Question, R.Answer>) {}
 }
