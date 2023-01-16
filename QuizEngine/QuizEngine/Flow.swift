1e242421a11800f974e0e9a08e1830f40c0ba6a3
AA <c6k3b@icloud.com>
2023-01-16 10:46:22 +0400

    Improved the property name

--- QuizEngine/QuizEngine/Flow.swift
+++ QuizEngine/QuizEngine/Flow.swift
@@ -4,7 +4,7 @@ class Flow<Delegate: QuizDelegate> {
 
     private let delegate: Delegate
     private let questions: [Question]
-    private var newAnswers: [(Question, Answer)] = []
+    private var answers: [(Question, Answer)] = []
 
     init(questions: [Question], delegate: Delegate) {
         self.questions = questions
@@ -22,7 +22,7 @@ private extension Flow {
             let question = questions[index]
             delegate.answer(for: question, completion: answer(for: question, at: index))
         } else {
-            delegate.didCompleteQuiz(withAnswers: newAnswers)
+            delegate.didCompleteQuiz(withAnswers: answers)
         }
     }
 
@@ -32,7 +32,7 @@ private extension Flow {
 
     func answer(for question: Question, at index: Int) -> (Answer) -> Void {
         return { [weak self] answer in
-            self?.newAnswers.replaceOrInsert((question, answer), at: index)
+            self?.answers.replaceOrInsert((question, answer), at: index)
             self?.delegateQuestionHandling(after: index)
         }
     }
