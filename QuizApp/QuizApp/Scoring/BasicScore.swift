8d4ddc7e5b936a937f58915acd121a5d546568e5
AA <c6k3b@icloud.com>
2023-01-16 21:55:42 +0400

    Makes the BasicScore works with generic types

--- QuizApp/QuizApp/Scoring/BasicScore.swift
+++ QuizApp/QuizApp/Scoring/BasicScore.swift
@@ -2,7 +2,7 @@
 //  Created by aa on 1/16/23.
 
 final class BasicScore {
-    static func score(for answers: [String], comparingTo correctAnswers: [String]) -> Int {
+    static func score<T: Equatable>(for answers: [T], comparingTo correctAnswers: [T]) -> Int {
         return zip(answers, correctAnswers).reduce(0) { score, tuple in
             return score + (tuple.0 == tuple.1 ? 1 : 0)
         }
