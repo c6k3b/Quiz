1ac727abff7b51f19f36cb59b6c7fa46a9c6aed4
AA <c6k3b@icloud.com>
2023-01-18 01:21:26 +0400

    Removed deprecated API methods

--- QuizApp/QuizAppTests/Routing/Helper/ResultHelper.swift
+++ QuizApp/QuizAppTests/Routing/Helper/ResultHelper.swift
@@ -1,23 +1,2 @@
 //  ResultHelper.swift
 //  Created by aa on 11/10/22.
-
-@testable import QuizEngine
-
-extension Result {
-    static func make(answers: [Question: Answer] = [:], score: Int = 0) -> Result {
-        return Result(answers: answers, score: score)
-    }
-}
-
-extension Result: Equatable where Answer: Equatable {
-   public static func == (lhs: Result, rhs: Result) -> Bool {
-       return lhs.score == rhs.score && lhs.answers == rhs.answers
-   }
-}
-
-extension Result: Hashable where Answer: Hashable {
-    public func hash(into hasher: inout Hasher) {
-        hasher.combine(answers)
-        hasher.combine(score)
-    }
-}
