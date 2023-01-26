8fb939194c151337cd6d51799c6968b1263e992c
AA <c6k3b@icloud.com>
2023-01-26 11:23:12 +0400

    Add single answer question enforcing invariants

--- QuizApp/QuizAppTests/Scoring/BasicQuizBuilderTest.swift
+++ QuizApp/QuizAppTests/Scoring/BasicQuizBuilderTest.swift
@@ -17,9 +17,9 @@ struct NonEmptyOptions {
 }
 
 struct BasicQuizBuilder {
-	private let questions: [Question<String>]
-	private let options: [Question<String> : [String]]
-	private let correctAnswers: [(Question<String>, [String])]
+	private var questions: [Question<String>]
+	private var options: [Question<String> : [String]]
+	private var correctAnswers: [(Question<String>, [String])]
 	
 	enum AddingError: Equatable, Error {
 		case duplicatedOptions([String])
@@ -41,6 +41,26 @@ struct BasicQuizBuilder {
 		self.questions = [question]
 		self.options = [question: options]
 		self.correctAnswers = [(question, [answer])]
+	}
+	
+	mutating func add(singleAnswerQuestion: String, options: NonEmptyOptions, answer: String) throws {
+		let question = Question.singleAnswer(singleAnswerQuestion)
+		let options = options.all
+
+		guard options.contains(answer) else {
+			throw AddingError.missingAnswerInOptions(answer: [answer], options: options)
+		}
+
+		guard Set(options).count == options.count else {
+			throw AddingError.duplicatedOptions(options )
+		}
+
+		var newOptions = self.options
+		newOptions[question] = options
+		
+		self.questions += [question]
+		self.options = newOptions
+		self.correctAnswers += [(question, [answer])]
 	}
 	
 	func build() -> BasicQuiz {
@@ -91,6 +111,80 @@ class BasicQuizBuilderTest: XCTestCase {
 				BasicQuizBuilder.AddingError.missingAnswerInOptions(
 					answer: ["A4"],
 					options: ["A1", "A1", "A3"]
+				)
+			)
+		}
+	}
+	
+	func test_addWithSingleAnswerQuestion() throws {
+		var sut = try BasicQuizBuilder(
+			singleAnswerQuestion: "Q1",
+			options: NonEmptyOptions(head: "A1", tail: ["A2", "A3"]),
+			answer: "A1"
+		)
+		
+		try sut.add(
+			singleAnswerQuestion: "Q2",
+			options: NonEmptyOptions(head: "A4", tail: ["A5", "A6"]),
+			answer: "A4"
+		)
+		
+		let result = sut.build()
+		
+		XCTAssertEqual(result.questions, [
+			.singleAnswer("Q1"),
+			.singleAnswer("Q2")
+		])
+		XCTAssertEqual(result.options, [
+			.singleAnswer("Q1"): ["A1", "A2", "A3"],
+			.singleAnswer("Q2"): ["A4", "A5", "A6"]
+		])
+		assertEqual(result.correctAnswers, [
+			(.singleAnswer("Q1"), ["A1"]),
+			(.singleAnswer("Q2"), ["A4"])
+		])
+	}
+	
+	func test_addWithSingleAnswerQuestion_duplicateOptions_throw() throws {
+		var sut = try BasicQuizBuilder(
+			singleAnswerQuestion: "Q1",
+			options: NonEmptyOptions(head: "A1", tail: ["A2", "A3"]),
+			answer: "A1"
+		)
+		
+		XCTAssertThrowsError(
+			try sut.add(
+				singleAnswerQuestion: "Q2",
+				options: NonEmptyOptions(head: "A4", tail: ["A4", "A6"]),
+				answer: "A4"
+			)
+		) { error in
+			XCTAssertEqual(
+				error as? BasicQuizBuilder.AddingError,
+				BasicQuizBuilder.AddingError.duplicatedOptions(["A4", "A4", "A6"])
+			)
+		}
+	}
+	
+	func test_addWithSingleAnswerQuestion_missingAnswerInOptions_throw() throws {
+		var sut = try BasicQuizBuilder(
+			singleAnswerQuestion: "Q1",
+			options: NonEmptyOptions(head: "A1", tail: ["A2", "A3"]),
+			answer: "A1"
+		)
+		
+		XCTAssertThrowsError(
+			try sut.add(
+				singleAnswerQuestion: "Q2",
+				options: NonEmptyOptions(head: "A4", tail: ["A5", "A6"]),
+				answer: "A7"
+			)
+		) { error in
+			XCTAssertEqual(
+				error as? BasicQuizBuilder.AddingError,
+				BasicQuizBuilder.AddingError.missingAnswerInOptions(
+					answer: ["A7"],
+					options: ["A4", "A5", "A6"]
 				)
 			)
 		}
